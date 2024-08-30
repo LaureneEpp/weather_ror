56f2fbd13a738904bfdc3409a5bf9e17c1d2fd05
Laurène Eppellé <Laurene@MacBook-Pro-de-Eppelle.local>
2024-08-30 22:15:04 +0200

    feat(weather): display API request

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -3,5 +3,18 @@ class WeatherController < ApplicationController
   end
 
   def fetch_weather
+    city = params[:city]
+    api_key = 'a6cbbf1ad1acfdc008e9c17f33b0f05a'
+
+    url = "https://api.openweathermap.org/data/2.5/weather?q=#{city}&appid=#{api_key}"
+    response = HTTParty.get(url)
+
+    if response.code == 200
+      weather_data = JSON.parse(response.body)
+      render json: weather_data
+    else
+      error_message = 'Impossible to fetch weather data'
+      render json: { error: error_message }, status: :unprocessable_entity
+    end
   end
 end
